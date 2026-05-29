a2e2ddd50a7f29705bed6a2c645c5b0f7fd0ca59
David <davideboren@gmail.com>
2026-05-28 22:01:49 -0500

    Rogue style room ASCII

--- src/Map.cpp
+++ src/Map.cpp
@@ -9,7 +9,7 @@
 Map::Map(){
     for(int y = 0; y < MAP_HEIGHT; y++){
         for(int x = 0; x < MAP_WIDTH; x++){
-            tiles[y][x].ch = '#';
+            tiles[y][x].ch = ' ';
             tiles[y][x].color = COLOR_PAIR(VISIBLE_COLOR);
             tiles[y][x].walkable = false;
             tiles[y][x].transparent = false;
@@ -33,8 +33,22 @@ Room Map::createRoom(int y, int x, int height, int width){
 }
 
 void Map::placeRoom(Room& room){
-   for(int y = room.pos.y; y < room.pos.y + room.height; y++){
-        for(int x = room.pos.x; x < room.pos.x + room.width; x++){
+    for(int y = room.pos.y; y < room.pos.y + room.height; y++){
+        tiles[y][room.pos.x].ch = '|';
+        tiles[y][room.pos.x + room.width].ch = '|';
+    }
+    for(int x = room.pos.x; x < room.pos.x + room.width + 1; x++){
+        tiles[room.pos.y][x].ch = '-';
+        tiles[room.pos.y + room.height][x].ch = '-';
+    }
+
+    tiles[room.pos.y][room.pos.x].ch = 'x';
+    tiles[room.pos.y + room.height][room.pos.x].ch = 'x';
+    tiles[room.pos.y][room.pos.x + room.width].ch = 'x';
+    tiles[room.pos.y + room.height][room.pos.x + room.width].ch = 'x';
+
+    for(int y = room.pos.y + 1; y < room.pos.y + room.height; y++){
+        for(int x = room.pos.x + 1; x < room.pos.x + room.width; x++){
             tiles[y][x].ch = '.';
             tiles[y][x].walkable = true;
             tiles[y][x].transparent = true;
@@ -42,6 +56,7 @@ void Map::placeRoom(Room& room){
    }
 }
 
+/*
 void Map::createRooms(){
     std::random_device rd;
     std::mt19937 gen(rd());
@@ -54,7 +69,7 @@ void Map::createRooms(){
        int height = distr(gen) % 10 + 10;
     }
 
-}
+}*/
 
 void Map::connectPoints(Position c1, Position c2){
     Position temp;
@@ -74,7 +89,17 @@ void Map::connectPoints(Position c1, Position c2){
             break;
         }
 
-        tiles[temp.y][temp.x].ch = '.';
+        Tile cur_tile = tiles[temp.y][temp.x];
+
+        if(cur_tile.ch == ' '){
+            tiles[temp.y][temp.x].ch = '#';
+        } else if(cur_tile.ch == '|' || cur_tile.ch == '-'){
+            tiles[temp.y][temp.x].ch = '+';
+            tiles[temp.y][temp.x].transparent = false;
+            tiles[temp.y][temp.x].color = COLOR_PAIR(PLAYER_COLOR);
+        } else {
+            tiles[temp.y][temp.x].ch = '.';
+        }
         tiles[temp.y][temp.x].walkable = true;
         tiles[temp.y][temp.x].transparent = true;
     }
